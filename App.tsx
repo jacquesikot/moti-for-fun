a9952f60e265c1af574d93c8fc34190f3ab0632e
Jacques Ikot <jacquesikot@gmail.com>
2021-06-18 10:13:17 +0100

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useReducer } from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { MotiView } from 'moti'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+     <HelloWorld />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+function Shape() {
+  return (
+    <MotiView
+      from={{
+        opacity: 0,
+        scale: 0.5,
+      }}
+      animate={{
+        opacity: 1,
+        scale: 1,
+      }}
+      transition={{
+        type: 'timing',
+      }}
+      style={styles.shape}
+    />
+  )
+}
+
+function HelloWorld() {
+  const [visible, toggle] = useReducer((s) => !s, true)
+
+  return (
+    <Pressable onPress={toggle} style={styles.container}>
+      {visible && <Shape />}
+    </Pressable>
+  )
+}
+
 const styles = StyleSheet.create({
+  shape: {
+    justifyContent: 'center',
+    height: 250,
+    width: 250,
+    borderRadius: 25,
+    marginRight: 10,
+    backgroundColor: 'white',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#9c1aff',
   },
 });
